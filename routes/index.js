45af6fa09d74a407a280e2f2944efc49b1165404
Gabriel Felipe Werner <gabriel_gfw@hotmail.com>
2021-01-25 09:29:19 -0300

    add users route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("Welcome");
+    res.render('welcome');
 });
 
 router.get("/register", (req, res) => {
-    res.render("Register");
+    res.render('register');
 });
 
 module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+router.get("/login", (req, res) => {
+    res.render('login');
+});
+
+router.get("/register", (req, res) => {
+    res.render(register);
+});
+
+router.post("/register", (req, res) => {
+
+});
+
+router.post("/login", (req, res, next) => {
+
+});
+
+router.get("/logout", (req, res) => {
+
+});
+
+module.exports = router;
